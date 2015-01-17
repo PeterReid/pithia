119e6bcbc45d4ebf63f62f5071aba5cf9ae9052f
Peter Reid <peter.d.reid@gmail.com>
2015-01-17 18:47:30 -0500

    Draw a string in the example page

--- page/page.c
+++ page/page.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 
 static int get_input() {
   int x;
@@ -74,6 +75,43 @@ typedef struct screen {
   glyph gs[SCREEN_CELLS];
 } screen;
 
+uint8_t file_contents[] = "3.1415926 is PI ";
+
+uint32_t unicode_to_glyphcode(uint32_t unicode) {
+  if( unicode>='0' && unicode<='9' ){
+    return unicode - (uint32_t)'0' + 10;
+  }else if( unicode>='a' && unicode<='z' ){
+    return 0x1000 + ((unicode-'a')<<4);
+  }else if( unicode>='A' && unicode<='Z' ){
+    return 0x3000 + ((unicode-'A')<<4);
+  }else if( unicode==' ' ){
+    return 0;
+  }
+  switch( unicode ){
+    case '_': return 1;
+    case '-': return 2;
+    case '.': return 3;
+    case ',': return 4;
+    case '/': return 5;
+    case'\\': return 6;
+    case ':': return 7;
+    case ';': return 8;
+    case '~': return 9;
+  }
+  return 999;
+}
+
+//int read_utf8_char(int8_t
+void utf7_display(screen *dest, uint32_t x, uint32_t y, uint8_t *str, uint32_t len) {
+  uint32_t i;
+  uint32_t write_idx = dest->width*y + x;
+  for( i=0; i<len; i++ ){
+    glyph *g = &dest->gs[write_idx+i];
+    g->ch = unicode_to_glyphcode((uint32_t)str[i]);
+    g->fg = 0xff;
+    g->bg = 0;
+  }
+}
 
 int __start() {
   screen s;
@@ -81,10 +119,12 @@ int __start() {
   s.width = SCREEN_WIDTH;
   s.height = SCREEN_HEIGHT;
   for ( i=0; i<SCREEN_CELLS; i++ ){
-    s.gs[i].ch = 10+(i%5);
+    s.gs[i].ch = 10+(i%2);
     s.gs[i].fg = 0x00ff00;
     s.gs[i].bg = 0;
   }
+  
+  utf7_display(&s, 3, 3, file_contents, 16);
   
   while(1) {
     display_screen(&s.width);
